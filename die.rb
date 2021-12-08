6ea673636f41ceda9563d9e9c26ef86db68b0073
gkansala13 <kansalag@oregonstate.edu>
2021-12-08 12:03:37 -0800

    removed random commented code

--- die.rb
+++ die.rb
@@ -7,23 +7,14 @@ class Die
   def initialize(number_of_sides)
     @number_of_sides = number_of_sides
     @value = 1
-    @prng = Random.new
   end
 
   def roll!
-    self.value = prng.rand(1..number_of_sides)
+    self.value = rand(1..number_of_sides)
   end
 
   def makeRoll
-        roll!
-    end
-
-#
-#
-  # def to_s
-  #   value
-  # end
-#
-# end
+    roll!
+  end
 
 end

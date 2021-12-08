18d1f4ef7bc1dd8113a69e51436f7a31080aea3e
gkansala13 <kansalag@oregonstate.edu>
2021-12-08 12:09:19 -0800

    Removed more whitespace from main

--- main.rb
+++ main.rb
@@ -6,15 +6,9 @@ NUMBER_TILES = 9
 NUMBER_DICE = 2
 NUMBER_SIDES = 6
 
-
 dice= (1..NUMBER_DICE).map { |i| Die.new(NUMBER_SIDES) }
 box= Box.new(NUMBER_TILES)
-
 game=Game.new(box, dice)
-
-
-
-
 
 puts Game::WELCOME_MESSAGE
 game.start!
